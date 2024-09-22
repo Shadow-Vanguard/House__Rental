d854398ab4160b182a795230b3bff9c08a1bcbd8
hari892002 <haripriyaka2025@mca.ajce.in>
2024-09-23 03:05:58 +0530

    commit

--- online_house_rental/online_house_rental/settings.py
+++ online_house_rental/online_house_rental/settings.py
@@ -130,8 +130,9 @@ MEDIA_URL = '/media/'
 # For development, ensure the following is set
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+# settings.py
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
